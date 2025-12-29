e68a50e24df0d11c56fabb1487f219e599192b81
Eggsscrambled <102927928+eggsscrambled@users.noreply.github.com>
2025-12-29 16:05:40 -0700

    basic UI

---
+++ Square Up/Assets/UI/BasicUINav.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class BasicUINav : MonoBehaviour
+{
+
+    public GameObject oldPanel;
+    public GameObject newPanel;
+    public void NextPanel()
+    {
+        oldPanel.SetActive(false);
+        newPanel.SetActive(true);
+    }
+}
---
+++ Square Up/Assets/UI/ButtonAnimator.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonAnimator : MonoBehaviour
+{
+
+    public GameObject button;
+    public bool InButton;
+    private AudioSource audioSource;
+    public AudioClip Hover;
+    public AudioClip Press;
+
+
+    [Header("Size Change")]
+    public float SizeChangeSpeed;
+    public float targetSize;
+    public float baseSize;
+    public float ClcikSize;
+
+
+
+    private void Start()
+    {
+        audioSource = GameObject.Find("Main Camera").GetComponent<AudioSource>();
+    }
+    public void OnHover()
+    {
+
+            InButton = true;
+        audioSource.pitch = Random.Range(0.9f, 1.1f);
+        audioSource.PlayOneShot(Hover);
+
+
+    }
+
+    public void OnHoverExit()
+    {
+            InButton = false;
+    }
+
+
+    private void Update()
+    {
+
+
+        
+            if (InButton)
+            {
+                if (button.transform.localScale.x < targetSize && button.transform.localScale.y < targetSize)
+                {
+                     button.transform.localScale += new Vector3(SizeChangeSpeed, SizeChangeSpeed, 0) * Time.deltaTime;
+                }
+
+
+
+
+            }
+
+            if (!InButton)
+            {
+                if (button.transform.localScale.x > baseSize && button.transform.localScale.y > baseSize)
+                {
+                     button.transform.localScale -= new Vector3(SizeChangeSpeed, SizeChangeSpeed, 0) * Time.deltaTime;
+                }
+
+
+
+
+            }
+
+        
+
+
+    }
+
+    public void pressed()
+    {
+        InButton = false ;
+        button.transform.localScale = new Vector3(1, 1, 1);
+        audioSource.PlayOneShot(Press);
+    }
+}
